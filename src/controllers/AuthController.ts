ba305309d5ade68360a64d4c8f376aadc0e48dbb
Manuj Gogoi <manujgogoi@gmail.com>
2023-12-23 21:28:11 +0530

    AuthController updated

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -117,6 +117,7 @@ export const login = async (req: express.Request, res: express.Response) => {
                 res.status(200).json({
                     "success": true,
                     "message": "Login successful",
+                    "data" : accessPayload
                 });
             } catch (error) {
                 console.log("Login error (JWT): ", error);
